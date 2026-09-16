423d16efdcab15a4e79d310ca27b0a47d51f78e6
Alla Mozharova <alik19.09@mail.ru>
2026-09-16 19:00:21 +0300

    H-1 Выполнить базовую настройку проекта

---
+++ Helper/Helper/App/SceneDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  SceneDelegate.swift
+//  Helper
+//
+//  Created by Алла on 16.09.2026.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        
+        let window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        
+        self.window = window
+    }
+}
+
---
+++ Helper/Helper/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Helper
+//
+//  Created by Алла on 16.09.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
